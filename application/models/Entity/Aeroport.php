3fac2f48b71384893be7a3a20f1685625c187b83
Basrak <ekar@hotmail.fr>
2018-01-22 17:11:37 +0100

    projet reformating controllers (encours) 2

--- application/models/Entity/Aeroport.php
+++ application/models/Entity/Aeroport.php
@@ -23,22 +23,7 @@ class Aeroport implements JsonSerializable {
      */
     private $_villeArpt;
        
-    //hydratation des données à partir de la base de données
-    public function hydrate(array $data)
-    {
-    foreach ($data as $key => $value)
-        {
-        // On récupère le nom du setter correspondant à l'attribut.
-        $method = 'set'.ucfirst($key);       
-        // Si le setter correspondant existe.
-        if (method_exists($this, $method))
-                {
-                // On appelle le setter.
-                $this->$method($value);
-                }
-        }
-    }       
-            
+         
     //Getters
     public function getIdArpt()  { return $this->_idArpt; }
     public function getNomArpt()  { return $this->_nomArpt; }
--- application/models/Entity/VolGen.php
+++ application/models/Entity/VolGen.php
@@ -40,23 +40,7 @@ class VolGen implements JsonSerializable
      */
     private $_jourVol;
       
-    
-    //hydratation des données à partir de la base de données
-    public function hydrate(array $data)
-    {
-    foreach ($data as $key => $value)
-        {
-        // On récupère le nom du setter correspondant à l'attribut.
-        $method = 'set'.ucfirst($key);       
-        // Si le setter correspondant existe.
-        if (method_exists($this, $method))
-                {
-                // On appelle le setter.
-                $this->$method($value);
-                }
-        }
-    }       
-            
+          
     //Getters
     public function getIdVol()  { return $this->_idVol; }
     public function getCodeVol()  { return $this->_codeVol; }
